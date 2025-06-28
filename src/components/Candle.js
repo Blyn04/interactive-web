690d4f6fcf7559add98fe9304dffbb6b991de77e
berlene <berlenebernabe12@gmail.com>
2025-06-28 21:01:37 +0800

    UPDATED UI

--- src/components/Candle.js
+++ src/components/Candle.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './styles/Candle.css';
+import React from "react";
+import "./styles/Candle.css";
 
-const Candle = ({ isBlownOut }) => {
-  return (
-    <div className="candle-container">
-      <div className={`candle ${isBlownOut ? 'blown-out' : ''}`}>
-        <div className="flame"></div>
+const Candle = ({ isBlownOut = false }) => (
+  <div className="cake-container">
+    <div className="cake">
+      <div className="cake-layer layer-bottom"></div>
+      <div className="cake-layer layer-middle"></div>
+      <div className="cake-layer layer-top">
+        <div className="cake-frosting"></div>
       </div>
     </div>
-  );
-};
+
+    <div className="candle-container">
+      <div className={`candle ${isBlownOut ? "blown-out" : ""}`}>
+        <div className="flame"></div>
+        <div className="flame-glow"></div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Candle;
