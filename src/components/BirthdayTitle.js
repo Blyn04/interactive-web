b5e1581ea2c29c3946d7e87a55e70234d570ef97
berlene <berlenebernabe12@gmail.com>
2025-06-30 18:09:05 +0800

    UPDATE TEXT

--- src/components/BirthdayTitle.js
+++ src/components/BirthdayTitle.js
@@ -4,14 +4,9 @@ import "./styles/BirthdayTitle.css";
 const text = "Happy Birthday!";
 
 const BirthdayTitle = () => (
-  <h1 className="birthday-title">
-    {Array.from(text).map((ch, i) => (
-      <span key={i} style={{ "--delay": `${i * 0.08}s` }}>
-        {ch === " " ? "\u00A0" : ch}
-      </span>
-    ))}
-    <span className="cat-emoji"> 🐱</span>
-  </h1>
+    <div class="candle-count-display">
+      HAPPY BIRTHDAY !
+    </div>
 );
 
 export default BirthdayTitle;
